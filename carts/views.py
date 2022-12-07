f5a0d421d1dd42d70287adf65d84b40c6aff5490
tuitenanevo <phanan1309@gmail.com>
2022-12-07 20:08:30 +0700

    adding more functionality of store and carts

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,8 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from .models import Cart, CartItem
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -13,6 +13,7 @@ def _cart_id(request):
         cart = request.session.create()
     return cart
 
+
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)  # Lấy Product
     product_variation = []  # Tạo mảng rỗng
@@ -22,58 +23,87 @@ def add_cart(request, product_id):
             value = request.POST[key]
 
             try:
-                variation = Variation.objects.get(product=product, product_variation__iexact=key, variation_value__iexact=value)
+                variation = Variation.objects.get(product=product, variation_category__iexact=key,
+                                                  variation_value__iexact=value)
                 product_variation.append(variation)
             except:
                 pass
 
-    # product = Product.objects.get(id=product_id)  # Lấy Product
+    product = Product.objects.get(id=product_id)  # Lấy Product
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))  # Lấy Cart bằng cách lấy cart_id từ session
     except Cart.DoesNotExist:
         cart = Cart.objects.create(
             cart_id=_cart_id(request)
         )
-        cart.save()
+    cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        if len(product_variation) > 0:
-            cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
-        cart_item.quantity += 1  # cart_item.quantity = cart_item.quantity + 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(product=product, cart=cart)
+        # existing_variation -> database
+        # current variations -> product_variation
+        # item_id -> database
+        ex_var_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+
+        print(ex_var_list)
+
+        # kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+        if product_variation in ex_var_list:
+            # tăng số lượng sản phẩm
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+
+        else:
+            # thêm sản phẩm mới vào giỏ hàng
+            item = CartItem.objects.create(product=product, quantity=1, cart=cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
+    else:
         cart_item = CartItem.objects.create(
-            product = product,
-            quantity = 1,
-            cart = cart,
+            product=product,
+            quantity=1,
+            cart=cart,
         )
         if len(product_variation) > 0:
             cart_item.variations.clear()
-            for item in product_variation:
-                cart_item.variations.add(item)
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     return redirect('cart')
 
-def remove_cart(request, product_id):
+
+def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect('cart')
 
-def remove_cart_item(request, product_id):
+
+def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
+
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
@@ -87,7 +117,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         grand_total = total
 
     except ObjectDoesNotExist:
-        pass # Không có gì cả
+        pass  # Không có gì cả
 
     context = {
         'total': total,
@@ -96,6 +126,5 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'tax': tax,
         'grand_total': grand_total,
     }
-
 
     return render(request, 'store/cart.html', context)
--- store/models.py
+++ store/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Product (models.Model):
+class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
@@ -23,6 +23,7 @@ class Product (models.Model):
     def __str__(self):
         return self.product_name
 
+
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
@@ -30,14 +31,16 @@ class VariationManager(models.Manager):
     def options(self):
         return super(VariationManager, self).filter(variation_category='option', is_active=True)
 
+
 variation_category_choice = (
     ('color', 'color'),
+    ('option', 'option'),
 )
+
 
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    variation_category = models.CharField(max_length=100,choices=variation_category_choice)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
     variation_value = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
